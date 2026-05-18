da8f1c23c50df18a59013f937fce36bacb3c6167
Caitlin Ripper <caitl@MacBook-Pro-2.local>
2026-05-18 12:35:48 +1000

    Add United app link, laundromats, fix image sizing

--- tour-guide/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TourGuide from './TourGuide.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TourGuide />
-  </React.StrictMode>,
-)
